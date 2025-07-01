2420cbf413b598da806135db36ee0a6fdb17d475
chailykevin <chailykevin@gmail.com>
2025-07-01 13:09:02 +0700

    feat: login api

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as Yup from "yup";
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
 
 type TRegister = {
   fullName: string;
@@ -8,6 +9,11 @@ type TRegister = {
   email: string;
   password: string;
   confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
 };
 
 const registerValidateSchema = Yup.object({
@@ -50,6 +56,54 @@ export default {
 
       res.status(400).json({
         meessage: err.message,
+        data: null,
+      });
+    }
+  },
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+    try {
+      // Ngequery identifier
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            username: identifier,
+          },
+        ],
+      });
+
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "user not found",
+          data: {
+            identifier,
+            password,
+          },
+        });
+      }
+
+      // Validasi password
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null,
+        });
+      }
+
+      res.status(200).json({
+        message: "Login success!",
+        data: userByIdentifier,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
         data: null,
       });
     }
